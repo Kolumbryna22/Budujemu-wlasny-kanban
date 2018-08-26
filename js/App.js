84c9d1351745c835da9b68fffcb53f6eeea18b17
Dominik Wilusz <wilusz1@o2.pl>
2018-08-26 16:02:48 +0200

    Update kanbad

--- js/App.js
+++ js/App.js
@@ -17,7 +17,8 @@ $.ajax({
 });
 
 function setupColumns(columns) {
-    columns.forEach(function (column){
+    console.log(columns);
+    columns.forEach(function(column){
         var col = new Column(column.id, column.name);
         board.createColumn(col);
         setupCards(col, column.cards);
@@ -25,7 +26,7 @@ function setupColumns(columns) {
 };
 
 function setupCards(col, cards) {
-    cards.forEach(function (card) {
+    cards.forEach(function(card) {
         var cardObj = new Card(card.id, card.name, card.bootcamp_kanban_column_id);
         col.createCard(cardObj);
     });
